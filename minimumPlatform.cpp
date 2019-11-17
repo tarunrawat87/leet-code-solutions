140357acec817a8ee7445c0a6f648a984494ff05
Tarun Rawat <tarunrawat8787@gmail.com>
2019-11-17 23:18:10 +0530

    completed:minimumPlatform

--- minimumPlatform.cpp
+++ minimumPlatform.cpp
@@ -4,7 +4,15 @@
 #include<sstream>
 #include<vector>
 #include <bits/stdc++.h> 
+#include<list>
 using namespace std;
+
+/*
+question link : https://practice.geeksforgeeks.org/problems/minimum-platforms/0
+
+
+
+*/
 
 int convert(char *start,char *end){
 string time_str(start,end);
@@ -14,6 +22,26 @@ str_stream>>time;
 return time;
 }
 
+bool leftStation(pair<int ,int> trainAtPltfrm,pair<pair< int, int> ,pair<int ,int> >  inComingTrain){
+//printf("incoming train :%d %d pltfrm train %d %d\n",inComingTrain.first.first,inComingTrain.first.second,trainAtPltfrm.first,trainAtPltfrm.second);
+if(inComingTrain.first.first>trainAtPltfrm.first)return true;
+if(trainAtPltfrm.first>inComingTrain.first.first){
+return false;
+}else{
+if(trainAtPltfrm.first==inComingTrain.first.first){
+return trainAtPltfrm.second<inComingTrain.first.second;
+}else{
+return false;
+}
+
+}
+if(trainAtPltfrm.first>inComingTrain.first.first&&trainAtPltfrm.second>inComingTrain.first.second)return false;
+
+
+return true;
+}
+
+
 bool sortFunc(pair<pair< int, int> ,pair<int ,int> > train1,pair<pair<int, int> ,pair<int ,int> > train2){
 if(train1.first.first==train2.first.first){
 return train1.first.second<train2.first.second;
@@ -21,10 +49,27 @@ return train1.first.second<train2.first.second;
 return train1.first.first<train2.first.first;
 }}
 
-bool statitionIsEmpty(vector<pair<pair< int, int> ,pair<int ,int> >  > trains,vector<pair<int ,int> > *trains_at_ptlfm){
+bool statitionIsEmpty(pair<pair< int, int> ,pair<int ,int> >  train,list<pair<int ,int> > *trains_at_ptlfm){
 
+if(trains_at_ptlfm->empty()){
+trains_at_ptlfm->push_back(make_pair(train.second.first,train.second.second));
+return false;
+}
 
+int index=0;
+list<pair<int ,int> > ::iterator i;
 
+for(i=trains_at_ptlfm->begin();i!=trains_at_ptlfm->end();i++){
+
+if(leftStation(*i,train)){
+trains_at_ptlfm->erase(i);
+trains_at_ptlfm->push_back(make_pair(train.second.first,train.second.second));
+return true;
+}
+
+}
+trains_at_ptlfm->push_back(make_pair(train.second.first,train.second.second));
+return false;
 }
 
 int main(){
@@ -60,20 +105,20 @@ i->second.second=convert(time+2,time+4);//pass Min here
 //sort your trains vector here
 sort(trains.begin(),trains.end(),sortFunc);
 
-vector<pair<int ,int> > trains_at_pltfrms;
+list<pair<int ,int> > trains_at_pltfrms;
 int stations=0;
 index=0;
 while(index<size){
 
-if(statitionIsEmpty(trains[index],trains_at_pltfrms)){
-trains_at_pltfrms.add();
+if(statitionIsEmpty(trains[index],&trains_at_pltfrms)){
+//trains_at_pltfrms.add();
 }else{
 stations++;
 }
 
 index++;
 }
-
+printf("%d\n",stations);
 }
 return 0;
 }
