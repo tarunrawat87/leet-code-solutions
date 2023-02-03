4e76af6fc9d47076ce5283f064518ac33e8c1c23
Tarun Rawat <tarunrawat8787@Gmail.com>
2023-02-03 22:12:47 +0530

    https://leetcode.com/problems/gas-station
    https://leetcode.com/problems/gas-station

--- gas-station.js
+++ gas-station.js
@@ -3,8 +3,29 @@
  * @param {number[]} cost
  * @return {number}
  */
+/*
+
+The idea is pretty simple ,
+
+1. Calculate the net cost of a station : net cost of a station is cost a vehicle has to bear when visting a station 
+net cost = gas[index] - cost[index];
+2. If a station has net cost as negative that means it can never be starting point 
+
+
+Here main thing to remember to think is if you start from station and while moving if at any point net sum(fuel) becomes zero , that means any station
+the  has come in the way can't be starting station . This is main crux of the problem.
+
+
+
+
+
+**/
+
+
+
 var canCompleteCircuit = function (gas, cost) {
 
+    //calculate netcost of all stations
     const stations = gas.map((ele, index) => {
         return {
             cost: (gas[index] - cost[index]),
@@ -15,15 +36,15 @@ var canCompleteCircuit = function (gas, cost) {
     const isViable = (index) => {
 
         let steps = gas.length;
-        let sum = 0;
+        let sum = 0;// consider this as total fuel.
         while (steps > 0) {
 
-            if (index === gas.length) index = 0;
-            sum += stations[index].cost;
-            if (sum < 0) return index;
-            stations[index].visited = true;
+            if (index === gas.length) index = 0;//if while moving you have come to end of array, make index = 0 just to move from start
+            sum += stations[index].cost; //calculate the net feul
+            if (sum < 0) return index;//if fuel is negative, can't move forward
+            stations[index].visited = true;//mark all index as visited as they come in path
             index++;
-            steps--;
+            steps--; 
         }
 
 
@@ -38,9 +59,8 @@ var canCompleteCircuit = function (gas, cost) {
 
         if (stations[index].cost >= 0) {
 
-            if (stations[index].visited === false) {
+            if (stations[index].visited === false) //is a index is not yet visited {
                 let newIndex = isViable(index);
-                console.log(newIndex);
                 if (newIndex === -1) return index;
             }
 
