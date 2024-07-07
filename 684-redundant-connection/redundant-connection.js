5ce0b2a62190d20dce81903164bc31ca516fd584
Tarun Rawat <tarunrawat8787@Gmail.com>
2024-07-08 01:44:31 +0530

    Time: 64 ms (64.10%) | Memory: 52.2 MB (65.81%) - LeetSync

--- 684-redundant-connection/redundant-connection.js
+++ 684-redundant-connection/redundant-connection.js
@@ -8,7 +8,7 @@ var findRedundantConnection = function (edges) {
     parents[0] = -1;
     edges.forEach((edge) => {
         edge.forEach((node) => {
-            console.log(node)
+            
             parents[node] = node;
         })
     });
@@ -24,8 +24,6 @@ var findRedundantConnection = function (edges) {
     const union = (node1, node2) => {
         let parent1 = find(node1);
         let parent2 = find(node2);
-        console.log('parent of ',node1,'is ',parent1);
-        console.log('parent of ',node2,'is ',parent2);
         
         if (parent1 !== parent2) {
             parents[parent2] = parent1;
@@ -39,7 +37,6 @@ var findRedundantConnection = function (edges) {
 
         let parent1 = find(edges[index][0]);
         let parent2 = find(edges[index][1]);
-        console.log('parent1 ',parent1,'parent2',parent2);
         
         if(parent1 === parent2){
             result = edges[index];
